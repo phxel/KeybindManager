94ebe6e313626b9e0913dce6c37b33d3e7f9e1ac
thatrtxdude <phillip.oertel.hamm@gmail.com>
2024-06-08 18:43:52 +0200

    implement blacklist bypass for toggle

--- lua/keybind_manager/core/server_core.lua
+++ lua/keybind_manager/core/server_core.lua
@@ -17,9 +17,36 @@ net.Receive("KeybindManager_ExecuteCommand", function(len, ply)
             end
             local cmd = table.remove(args, 1)
 
-            -- Run the command with arguments
             if cmd then
-                RunConsoleCommand(cmd, unpack(args))
+                if cmd == "toggle" and #args == 3 then
+                    local convar_name = args[1]
+                    local value1 = tonumber(args[2])
+                    local value2 = tonumber(args[3])
+                    if convar_name and value1 and value2 then
+                        local var = GetConVar(convar_name)
+                        if var then
+                            local currentValue = var:GetInt()
+                            local newValue = (currentValue == value1) and value2 or value1
+                            RunConsoleCommand(convar_name, tostring(newValue))
+                        else
+                            print("[KeybindManager] Unknown ConVar for toggle: " .. convar_name)
+                        end
+                    else
+                        print("[KeybindManager] Invalid arguments for toggle")
+                    end
+                else
+                    -- Handle other commands using RunConsoleCommand
+                    local success, err = pcall(function()
+                        RunConsoleCommand(cmd, unpack(args))
+                    end)
+
+                    if not success then
+                        print("[KeybindManager] Failed to run command with RunConsoleCommand: " .. err)
+                        ply:ConCommand(command)
+                    end
+                end
+            else
+                print("[KeybindManager] Command is invalid or empty.")
             end
         else
             ply:ConCommand(command)
@@ -28,5 +55,4 @@ net.Receive("KeybindManager_ExecuteCommand", function(len, ply)
         print("[KeybindManager] Invalid player or command received.")
     end
 end)
-
 
