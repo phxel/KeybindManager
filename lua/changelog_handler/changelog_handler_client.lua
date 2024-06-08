91be690bb304b59be4979454fde73d73a4202391
thatrtxdude <phillip.oertel.hamm@gmail.com>
2024-06-08 16:41:43 +0200

    changelogs stored in client database

--- lua/changelog_handler/changelog_handler_client.lua
+++ lua/changelog_handler/changelog_handler_client.lua
@@ -1,16 +1,41 @@
+-- Create a console variable to store the user's preference
+if not ConVarExists("show_changelogs") then
+    CreateConVar("show_changelogs", "1", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Toggle the display of changelogs")
+end
+
+-- Function to get the last seen version from the client
+local function GetLastSeenVersion()
+    return cookie.GetString("KeybindManager_LastSeenVersion", "0.0")
+end
+
+-- Function to mark changelogs as seen
+local function MarkChangelogAsSeen(version)
+    cookie.Set("KeybindManager_LastSeenVersion", version)
+end
+
 net.Receive("SendChangelog", function()
     if not GetConVar("show_changelogs"):GetBool() then return end
 
     local changelogs = net.ReadTable()
     if #changelogs > 0 then
-        chat.AddText("Recent changelogs for Keybind Manager:")
-        for _, log in ipairs(changelogs) do
-            chat.AddText(log)
+        local log = changelogs[1]  -- Use correct index
+
+        if log then
+            local lastSeenVersion = GetLastSeenVersion()
+
+            if log.version > lastSeenVersion then
+                chat.AddText("Recent changelogs for Keybind Manager:")
+                chat.AddText(log.log)  -- Make sure to access the log property
+                MarkChangelogAsSeen(log.version)
+            end
         end
     end
 end)
 
--- Create a console variable to store the user's preference
-if not ConVarExists("show_changelogs") then
-    CreateConVar("show_changelogs", "1", {FCVAR_ARCHIVE, FCVAR_REPLICATED}, "Toggle the display of changelogs")
-end
+-- Request changelogs from the server when the client joins
+hook.Add("InitPostEntity", "RequestChangelogOnJoin", function()
+    local lastSeenVersion = GetLastSeenVersion()
+    net.Start("SendChangelogRequest")
+    net.WriteString(lastSeenVersion)
+    net.SendToServer()
+end)
--- lua/changelog_handler/changelog_handler_server.lua
+++ lua/changelog_handler/changelog_handler_server.lua
@@ -1,65 +1,43 @@
+util.AddNetworkString("SendChangelog")
+util.AddNetworkString("SendChangelogRequest")
+
 ChangelogHandler = {}
 ChangelogHandler.Changelogs = {
     {version = "1.0.0-Stable", log = "Version 1.0.0 Stable - Initial release"},
-    {version = "1.0.1-Stable", log = "Version 1.0.1 Stable - Version Bump, nothing has changed"},
-    {version = "1.1.0-Stable", log = "Version 1.1.0 Stable - Remove isDefaultAction, not needed anymore"},
-    {version = "1.1.5-Stable", log = "Version 1.1.5 Stable - Added button to disable showing changelogs"},
 }
-ChangelogHandler.CurrentVersion = "1.1.5-Stable"
+ChangelogHandler.CurrentVersion = "1.0.0-Stable"
 
-util.AddNetworkString("SendChangelog")
+-- Function to send the most recent changelog to the client if it's newer than the last seen version
+function ChangelogHandler:SendRecentChangelog(ply, lastSeenVersion)
+    local mostRecentChangelog = self.Changelogs[#self.Changelogs]
 
--- Function to send recent changelogs to the client
-function ChangelogHandler:SendRecentChangelogs(ply, lastSeenVersion)
-    local recentChangelogs = {}
-    for _, entry in ipairs(self.Changelogs) do
-        if entry.version > lastSeenVersion then
-            table.insert(recentChangelogs, entry.log)
-        end
-    end
-
-    net.Start("SendChangelog")
-    net.WriteTable(recentChangelogs)
-    net.Send(ply)
-end
-
--- Check the last seen version
-function ChangelogHandler:GetLastSeenVersion(ply)
-    return ply:GetPData("ChangelogVersion", "0.0")
-end
-
-function ChangelogHandler:ClearChangelogEntries()
-    for _, ply in ipairs(player.GetAll()) do
-        ply:SetPData("ChangelogVersion", "0.0")
+    if mostRecentChangelog and mostRecentChangelog.version > lastSeenVersion then
+        net.Start("SendChangelog")
+        net.WriteTable({mostRecentChangelog})
+        net.Send(ply)
     end
 end
 
-
--- Mark changelog as seen for the player
-function ChangelogHandler:MarkChangelogAsSeen(ply)
-    ply:SetPData("ChangelogVersion", self.CurrentVersion)
-end
-
--- Handle player joining to send the changelog
-hook.Add("PlayerInitialSpawn", "SendChangelogOnJoin", function(ply)
-    local lastSeenVersion = ChangelogHandler:GetLastSeenVersion(ply)
-    ChangelogHandler:SendRecentChangelogs(ply, lastSeenVersion)
-    ChangelogHandler:MarkChangelogAsSeen(ply)
+-- Handle request from client for changelogs
+net.Receive("SendChangelogRequest", function(len, ply)
+    local lastSeenVersion = net.ReadString()
+    ChangelogHandler:SendRecentChangelog(ply, lastSeenVersion)
 end)
 
 -- Example command to manually trigger changelog (for testing)
 hook.Add("PlayerSay", "ShowChangelogCommand", function(ply, text)
     if text == "!changelog" then
-        local lastSeenVersion = ChangelogHandler:GetLastSeenVersion(ply)
-        ChangelogHandler:SendRecentChangelogs(ply, lastSeenVersion)
-        ChangelogHandler:MarkChangelogAsSeen(ply)
+        local lastSeenVersion = "0.0"
+        ChangelogHandler:SendRecentChangelog(ply, lastSeenVersion)
         return ""
     end
 end)
 
 concommand.Add("clear_changelog_entries", function(ply)
     if IsValid(ply) and ply:IsAdmin() then
-        ChangelogHandler:ClearChangelogEntries()
+        for _, player in ipairs(player.GetAll()) do
+            player:SendLua("cookie.Delete('KeybindManager_LastSeenVersion')")
+        end
         ply:ChatPrint("Changelog entries have been cleared for all players.")
     else
         print("Changelog entries have been cleared for all players.")
